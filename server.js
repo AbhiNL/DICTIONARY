6676842eda720de79dd44ebe5e058e51a7a3fab7
AbhiNL <96882416+AbhiNL@users.noreply.github.com>
2023-08-17 11:44:58 +0530

    Update server.js

--- server.js
+++ server.js
@@ -1,44 +1,37 @@
-const express = require('express')
+const url = "https://api.dictionaryapi.dev/api/v2/entries/en/";
+const result = document.getElementById("result");
+const sound = document.getElementById("sound");
+const btn = document.getElementById("search-btn");
 
-const axios = require("axios")
-
-const path =  require("path") 
-
-const app = express()
-
-const port = 3000
-
-app.get('/SEARCH WORD', (req, res) => {
-
-console.log(path.join(__dirname, 'public'))
-
-return res.sendFile('public/index.html' ,  {root: __dirname});
-
-})
-
-app.get('/searchword', (req, res) => { 
-  //res.send('Hello World! Harry') 
-  console.log(req.params)
-
-var options = {
-  method: 'GET',
-  url: 'https://mashape-community-urban-dictionary.p.rapidapi.com/define',
-  params: {term: 'old'},
-  headers: {
-    'X-RapidAPI-Key': 'd75e7c1e7cmsh026ae874f58d05ep1bce6djsn5c34b7593ef7',
-    'X-RapidAPI-Host': 'mashape-community-urban-dictionary.p.rapidapi.com'
-  }
-};
-
-axios.request(options).then(function (response) {
-  console.log(response.data);
-  res.json(response.data);
-}).catch(function (error) {
-  console.error(error);
- 
+btn.addEventListener("click", () => {
+    let inpWord = document.getElementById("inp-word").value;
+    fetch(`${url}${inpWord}`)
+        .then((response) => response.json())
+        .then((data) => {
+            console.log(data);
+            result.innerHTML = `
+            <div class="word">
+                    <h3>${inpWord}</h3>
+                    <button onclick="playSound()">
+                        <i class="fas fa-volume-up"></i>
+                    </button>
+                </div>
+                <div class="details">
+                    <p>${data[0].meanings[0].partOfSpeech}</p>
+                    <p>/${data[0].phonetic}/</p>
+                </div>
+                <p class="word-meaning">
+                   ${data[0].meanings[0].definitions[0].definition}
+                </p>
+                <p class="word-example">
+                    ${data[0].meanings[0].definitions[0].example || ""}
+                </p>`;
+            sound.setAttribute("src", `https:${data[0].phonetics[0].audio}`);
+        })
+        .catch(() => {
+            result.innerHTML = `<h3 class="error">Couldn't Find The Word</h3>`;
+        });
 });
-})
-
-app.listen(port, () => {
-  console.log(`Example app listening on port ${port}- http://localhost:3000`)
-})
+function playSound() {
+    sound.play();
+}
